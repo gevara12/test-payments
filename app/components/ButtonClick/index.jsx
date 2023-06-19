a8cb339cb3b654db2d5495d9394d130025a52449
atantsyra <aleksandr.tantsiura@mypatron.us>
2023-06-19 13:18:55 +0300

    test postmessage

--- app/components/ButtonClick/index.jsx
+++ app/components/ButtonClick/index.jsx
@@ -3,7 +3,8 @@
 export const ButtonClick = () => {
   const handleClick = () => {
     console.log('click');
-    window.ReactNativeWebView.postMessage('closeWebView');
+    // window.ReactNativeWebView.postMessage('closeWebView');
+    window.webkit.messageHandlers.jsMessageHandler.postMessage('closeWebView');
   };
   return <button onClick={handleClick}>Send postmessage</button>;
 };
